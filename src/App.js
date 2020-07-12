f337956dd16616e063b4a77fae43bf0ae34a8d0c
Chaim <cfinson@emergingmed.com>
2020-07-12 22:42:26 +0300

    initial setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import TopNavMenu from './navigation/TopNavMenu.js'
+
+
+
+function App() {
+  return (
+      <div className="App">
+        <TopNavMenu />
+      </div>
+  );
+}
+
+export default App;
